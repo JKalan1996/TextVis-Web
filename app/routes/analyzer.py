053b3fe6a03423280f4d9de25d6f7ae61bb76919
IDVXjzc <chjzcjames@gmail.com>
2019-09-09 18:01:07 +0800

    update

--- app/routes/analyzer.py
+++ app/routes/analyzer.py
@@ -9,7 +9,7 @@ def search(parameter):
     conn = psycopg2.connect(database= database_name, user="postgres", password="TJ.VisLab", host="202.120.188.26",port="5432")
     cur = conn.cursor()
 
-    cur.execute("select name from videos where name ~ '"+parameter['txt']+"' and year between"+parameter['year'][0]+' and '+parameter['year'][1])
+    cur.execute("select distinct name from videos where name ~ '"+parameter['txt']+"' and year between"+parameter['year'][0]+' and '+parameter['year'][1])
 
     result = cur.fetchall()
 
